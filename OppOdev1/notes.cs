5c139373299425a0989a957cc84421ad12fdda65
Halil Zilif <halilzilif12@gmail.com>
2024-01-16 22:29:25 +0300

    update

--- OppOdev1/notes.cs
+++ OppOdev1/notes.cs
@@ -7,13 +7,12 @@ using System.Threading.Tasks;
 
 namespace OppOdev1
 {
-    public class Notes 
+    public class Notes
     {
         public int Id { get; set; }
-
         public int UserId { get; set; }
         public string Text { get; set; }
         public DateTime CreatedAt { get; set; }
-        
+
     }
 }
